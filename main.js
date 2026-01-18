7314f6d7371db34aa9638d56b2f65917e46b27bf
cfoforu <cfoforu@gmail.com>
2026-01-18 06:44:15 +0000

    Initial commit

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-// Add JS here
+const generateBtn = document.getElementById('generate-btn');
+const numbersDisplay = document.getElementById('numbers-display');
+
+generateBtn.addEventListener('click', () => {
+    numbersDisplay.innerHTML = '';
+    const numbers = new Set();
+    while (numbers.size < 6) {
+        numbers.add(Math.floor(Math.random() * 45) + 1);
+    }
+
+    for (const number of [...numbers].sort((a, b) => a - b)) {
+        const numberBall = document.createElement('div');
+        numberBall.classList.add('number-ball');
+        numberBall.textContent = number;
+        numbersDisplay.appendChild(numberBall);
+    }
+});
